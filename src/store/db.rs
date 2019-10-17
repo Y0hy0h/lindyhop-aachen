d85c150279c6d9b0047abdcc246ac0a6c47a71e4
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-10-17 11:13:38 +0200

    Add CRUD for Event and Occurrence

--- src/store/db.rs
+++ src/store/db.rs
@@ -1,4 +1,4 @@
-use juniper::{GraphQLInputObject, GraphQLObject};
+use juniper::{GraphQLInputObject};
 use rocket::Rocket;
 use rocket_contrib::databases::diesel;
 
@@ -64,8 +64,31 @@ pub struct Event {
     pub description: String,
 }
 
+#[derive(Clone, Debug, Insertable, AsChangeset, GraphQLInputObject)]
+#[table_name = "events"]
+pub struct NewEvent {
+    pub title: String,
+    pub teaser: String,
+    pub description: String,
+}
+
+#[derive(Clone, Debug, AsChangeset, GraphQLInputObject)]
+#[table_name = "events"]
+pub struct UpdateEvent {
+    pub title: Option<String>,
+    pub teaser: Option<String>,
+    pub description: Option<String>,
+}
+
 #[derive(
-    Queryable, Insertable, Clone, Debug, Identifiable, PartialEq, AsChangeset, Associations,
+    Queryable,
+    Insertable,
+    Clone,
+    Debug,
+    Identifiable,
+    PartialEq,
+    AsChangeset,
+    Associations,
 )]
 #[belongs_to(Event, foreign_key = "event_id")]
 #[belongs_to(Location, foreign_key = "location_id")]
@@ -78,7 +101,25 @@ pub struct Occurrence {
     pub location_id: Id,
 }
 
-#[derive(Queryable, Clone, Identifiable, Debug, GraphQLObject)]
+#[derive(Clone, Debug, Insertable, AsChangeset, GraphQLInputObject)]
+#[table_name = "occurrences"]
+pub struct NewOccurrence {
+    pub event_id: Id,
+    pub start: NaiveDateTime,
+    pub duration: i32,
+    pub location_id: Id,
+}
+
+#[derive(Clone, Debug, AsChangeset, GraphQLInputObject)]
+#[table_name = "occurrences"]
+pub struct UpdateOccurrence {
+    pub event_id: Option<Id>,
+    pub start: Option<NaiveDateTime>,
+    pub duration: Option<i32>,
+    pub location_id: Option<Id>,
+}
+
+#[derive(Queryable, Clone, Identifiable, Debug)]
 #[table_name = "locations"]
 pub struct Location {
     pub id: Id,
--- src/store/mod.rs
+++ src/store/mod.rs
@@ -1,11 +1,12 @@
 mod db;
 
+use chrono::NaiveDateTime;
 use diesel::prelude::*;
 use rocket::fairing::{self, Fairing};
 use rocket::request::{FromRequest, Outcome, Request};
 use rocket::Rocket;
 
-use db::{Id, Location, NewLocation, UpdateLocation};
+use db::*;
 
 pub type Schema = juniper::RootNode<'static, Query, Mutation>;
 
@@ -13,10 +14,96 @@ pub fn new_schema_instance() -> Schema {
     Schema::new(Query, Mutation)
 }
 
+#[juniper::object(Context=Store)]
+impl Event {
+    fn id(&self) -> Id {
+        self.id
+    }
+    fn title(&self) -> &String {
+        &self.title
+    }
+
+    fn teaser(&self) -> &String {
+        &self.teaser
+    }
+
+    fn description(&self) -> &String {
+        &self.description
+    }
+
+    fn occurrences(&self, context: &Store) -> Vec<Occurrence> {
+        use db::schema::occurrences::dsl::*;
+        occurrences
+            .filter(event_id.eq(self.id))
+            .load(&*context.0)
+            .expect("Error loading from database.")
+    }
+}
+
+#[juniper::object(Context=Store)]
+impl Occurrence {
+    fn id(&self) -> Id {
+        self.id
+    }
+
+    fn event(&self, context: &Store) -> Event {
+        use db::schema::events::dsl::*;
+        events
+            .find(self.event_id)
+            .first(&*context.0)
+            .expect("Error loading from database.")
+    }
+
+    fn start(&self) -> &NaiveDateTime {
+        &self.start
+    }
+    fn duration(&self) -> i32 {
+        self.duration
+    }
+
+    fn location(&self, context: &Store) -> Location {
+        use db::schema::locations::dsl::*;
+        locations
+            .find(self.location_id)
+            .first(&*context.0)
+            .expect("Error loading from database.")
+    }
+}
+
+#[juniper::object(Context=Store)]
+impl Location {
+    fn id(&self) -> Id {
+        self.id
+    }
+
+    fn name(&self) -> &String {
+        &self.name
+    }
+
+    fn address(&self) -> &String {
+        &self.address
+    }
+
+    fn occurrences(&self, context: &Store) -> Vec<Occurrence> {
+        use db::schema::occurrences::dsl::*;
+        occurrences
+            .filter(location_id.eq(self.id))
+            .load(&*context.0)
+            .expect("Error loading from database.")
+    }
+}
+
 pub struct Query;
 
 #[juniper::object(Context=Store)]
 impl Query {
+    fn all_events(context: &Store) -> Vec<Event> {
+        use db::schema::events::dsl::*;
+        events
+            .load(&*context.0)
+            .expect("Error loading from database.")
+    }
+
     fn all_locations(context: &Store) -> Vec<Location> {
         use db::schema::locations::dsl::*;
         locations
@@ -29,6 +116,84 @@ pub struct Mutation;
 
 #[juniper::object(Context=Store)]
 impl Mutation {
+    fn add_event(context: &Store, new_event: NewEvent) -> Id {
+        use db::schema::events::dsl::*;
+        context
+            .0
+            .transaction(|| {
+                diesel::insert_into(events)
+                    .values(&new_event)
+                    .execute(&*context.0)?;
+                events.select(id).order(id.desc()).first(&*context.0)
+            })
+            .expect("Error inserting into database.")
+    }
+
+    fn update_event(context: &Store, id_to_update: Id, new_event: UpdateEvent) -> Event {
+        use db::schema::events::dsl::*;
+        diesel::update(events.find(id_to_update))
+            .set(new_event)
+            .execute(&*context.0)
+            .expect("Error updating in database.");
+        events
+            .find(id_to_update)
+            .first(&*context.0)
+            .expect("Error fetching from database.")
+    }
+
+    fn remove_event(context: &Store, id_to_remove: Id) -> Event {
+        use db::schema::events::dsl::*;
+        let item = events
+            .find(id_to_remove)
+            .first(&*context.0)
+            .expect("Error fetching from database.");
+        diesel::delete(&item)
+            .execute(&*context.0)
+            .expect("Error deleting from database.");
+        item
+    }
+
+    fn add_occurrence(context: &Store, new_occurrence: NewOccurrence) -> Id {
+        use db::schema::occurrences::dsl::*;
+        context
+            .0
+            .transaction(|| {
+                diesel::insert_into(occurrences)
+                    .values(&new_occurrence)
+                    .execute(&*context.0)?;
+                occurrences.select(id).order(id.desc()).first(&*context.0)
+            })
+            .expect("Error inserting into database.")
+    }
+
+    fn update_occurrence(
+        context: &Store,
+        id_to_update: Id,
+        new_occurrence: UpdateOccurrence,
+    ) -> Occurrence {
+        use db::schema::occurrences::dsl::*;
+        diesel::update(occurrences.find(id_to_update))
+            .set(new_occurrence)
+            .execute(&*context.0)
+            .expect("Error updating in database.");
+        occurrences
+            .find(id_to_update)
+            .first(&*context.0)
+            .expect("Error fetching from database.")
+    }
+
+    fn remove_occurrence(context: &Store, id_to_remove: Id) -> Occurrence {
+        use db::schema::occurrences::dsl::*;
+        let item = occurrences
+            .find(id_to_remove)
+            .first(&*context.0)
+            .expect("Error fetching from database.");
+        diesel::delete(&item)
+            .execute(&*context.0)
+            .expect("Error deleting from database.");
+        item
+    }
+
     fn add_location(context: &Store, new_location: NewLocation) -> Id {
         use db::schema::locations::dsl::*;
         context
